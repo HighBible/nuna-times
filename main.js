67bbbd8b0ad5ab9cc0ca820f89639552b9b44f8e
Seonkyung Ko <stitch3514@gmail.com>
2024-02-20 21:46:13 +0900

    second commit

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ const API_KEY = 'bb51046ed57e470b8dfa522463d8ebc4'
 let news;
 const  getLatestNews = async () => {
   //new 인스턴스
-  const url = new URL (`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`)
+  // const url = new URL (`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`)
+  const url = new URL (`https://bible-nuna-times.netlify.app//top-headlines?country=kr&apiKey=${API_KEY}`)
   //fetch ; 해당 url에 데이터 요청
   const response = await fetch(url);
   const data = await response.json()

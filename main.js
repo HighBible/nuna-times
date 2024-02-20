355b41a8dc9d9dae778fde98c5e28c9f2c40140c
Seonkyung Ko <stitch3514@gmail.com>
2024-02-20 21:40:59 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+const API_KEY = 'bb51046ed57e470b8dfa522463d8ebc4'
+let news;
+const  getLatestNews = async () => {
+  //new 인스턴스
+  const url = new URL (`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`)
+  //fetch ; 해당 url에 데이터 요청
+  const response = await fetch(url);
+  const data = await response.json()
+
+  news = data.articles
+
+  console.log('dddd', news)
+}
+getLatestNews()
